c3a229789e175fe4829ff213fc4737cc67c394f3
duga9cu <asinochilegge87@yahoo.it>
2013-05-29 17:44:32 +0200

    grafica ridisegnata per leopard con problema del player sovrapposto

--- MicArrayAnalyzer/src/gui.h
+++ MicArrayAnalyzer/src/gui.h
@@ -1,14 +1,14 @@
 /**********************************************************************
-
-  Audacity: A Digital Audio Editor
-              
-  Microphone Array Analyzer
-  
-  gui.h
-
-  Luca Novarini
-
-**********************************************************************/
+ 
+ Audacity: A Digital Audio Editor
+ 
+ Microphone Array Analyzer
+ 
+ gui.h
+ 
+ Luca Novarini
+ 
+ **********************************************************************/
 
 
 #ifndef __MICARRAYANALYZER_GUI_H__
@@ -24,83 +24,88 @@
 // Some consts to define Main Dialog aspect
 const int DEFAULT_RULER_FORMAT = 0;          //0 = deg, 1 = pixel
 /*const int RULERS_FONT_SIZE = 7;              
-const wxCoord RULERS_MAIN_NOTCHES = 10;      //Length of main notches
-const wxCoord RULERS_SEC_NOTCHES = 5;        //Length of secondary notches
-const int RULERS_MAIN_DEG_XSUBDIV = 8;        //One subdivision every 45 degrees (considering X_RES_DEG = 360)
-const int RULERS_SEC_DEG_XSUBDIV = 4;         //Four secondary notches inside each 45 degrees interval.
-const int RULERS_MAIN_PX_XSUBDIV = 8;         //One subdivision every 120 pixels.
-const int RULERS_SEC_PX_XSUBDIV = 4;          //Four secondary notches inside each 120 px interval.
-const int RULERS_MAIN_DEG_YSUBDIV = 4;        //Same here, but for Y-Axis Ruler!
-const int RULERS_SEC_DEG_YSUBDIV = 4;         
-const int RULERS_MAIN_PX_YSUBDIV = 4;         
-const int RULERS_SEC_PX_YSUBDIV = 4;    
-*/
+ const wxCoord RULERS_MAIN_NOTCHES = 10;      //Length of main notches
+ const wxCoord RULERS_SEC_NOTCHES = 5;        //Length of secondary notches
+ const int RULERS_MAIN_DEG_XSUBDIV = 8;        //One subdivision every 45 degrees (considering X_RES_DEG = 360)
+ const int RULERS_SEC_DEG_XSUBDIV = 4;         //Four secondary notches inside each 45 degrees interval.
+ const int RULERS_MAIN_PX_XSUBDIV = 8;         //One subdivision every 120 pixels.
+ const int RULERS_SEC_PX_XSUBDIV = 4;          //Four secondary notches inside each 120 px interval.
+ const int RULERS_MAIN_DEG_YSUBDIV = 4;        //Same here, but for Y-Axis Ruler!
+ const int RULERS_SEC_DEG_YSUBDIV = 4;         
+ const int RULERS_MAIN_PX_YSUBDIV = 4;         
+ const int RULERS_SEC_PX_YSUBDIV = 4;    
+ */
 //----------------------------------------------------
 // Module Configuration Dialog
 //----------------------------------------------------
 class MicArrayAnalyzerConfDlg : public MyModuleConfDlg
-   {
-      MicArrayAnalyzer* mMAA;
-      wxStaticBitmap* wxsbHeadersCheck;
-      bool bHeaders, bBgndImage, bLength, bVirtMikes, bProjChannels, bProjRate, bProjBits;
-   protected:
-      void OnHelp( wxCommandEvent& event );
-      void OnOk( wxCommandEvent& event );
-      void OnCancel( wxCommandEvent& event );
-      void OnPaint(wxPaintEvent & event);
-      void Render(wxDC& dc);
-      void SetHeadersCheckIcon(const wxChar* icon);
-      void RePaint();
-      void EnableTable();
-      void OnBrowseXML(wxCommandEvent& event);
-      void OnBrowseBGND(wxCommandEvent& event);
-      void IsAllOKCheck();
-      void MinSPLOnFocus(wxFocusEvent& event);
-      void MinSPLKillFocus(wxFocusEvent& event);
-      void MinSPLOnChar(wxKeyEvent& event);
-      void FSOnFocus(wxFocusEvent& event);
-      void FSKillFocus(wxFocusEvent& event);
-      void FSOnChar(wxKeyEvent& event);
-      double ReadAndForceDoubleTextCtrl(wxTextCtrl *txt, const double def_val);
-   public:
-      // Constructor / Destructor
-      MicArrayAnalyzerConfDlg(wxWindow* parent, MicArrayAnalyzer* maa);
-      ~MicArrayAnalyzerConfDlg();
-   };
+	{
+		MicArrayAnalyzer* mMAA;
+		wxStaticBitmap* wxsbHeadersCheck;
+		bool bHeaders, bBgndImage, bLength, bVirtMikes, bProjChannels, bProjRate, bProjBits;
+	protected:
+		void OnHelp( wxCommandEvent& event );
+		void OnOk( wxCommandEvent& event );
+		void OnCancel( wxCommandEvent& event );
+		void OnPaint(wxPaintEvent & event);
+		void Render(wxDC& dc);
+		void SetHeadersCheckIcon(const wxChar* icon);
+		void RePaint();
+		void EnableTable();
+		void OnBrowseXML(wxCommandEvent& event);
+		void OnBrowseBGND(wxCommandEvent& event);
+		void IsAllOKCheck();
+		void MinSPLOnFocus(wxFocusEvent& event);
+		void MinSPLKillFocus(wxFocusEvent& event);
+		void MinSPLOnChar(wxKeyEvent& event);
+		void FSOnFocus(wxFocusEvent& event);
+		void FSKillFocus(wxFocusEvent& event);
+		void FSOnChar(wxKeyEvent& event);
+		double ReadAndForceDoubleTextCtrl(wxTextCtrl *txt, const double def_val);
+	public:
+		// Constructor / Destructor
+		MicArrayAnalyzerConfDlg(wxWindow* parent, MicArrayAnalyzer* maa);
+		~MicArrayAnalyzerConfDlg();
+	};
 
 
 //----------------------------------------------------------------------------
 // Showing Result Dialog
 //----------------------------------------------------------------------------
 class MicArrayAnalyzerDlg : public MyModuleDlg
-   {
-      MicArrayAnalyzer* mMAA;
-      
-      int m_iCurrRulersFormat; // used for toggling...
-          
-   protected:
-      void OnOk(wxCommandEvent& event);
-      void OnCopyResultsToClipboard(wxCommandEvent& event);
-//      void OnChangeRulersFormat(wxCommandEvent& event);
-      void OnSeparateBandAutoscale(wxCommandEvent& event);
-//      void OnEditWatchpoints(wxCommandEvent& event);
-      void OnShowVirtMikesPos(wxCommandEvent& event);
-      void OnScaleStyle(wxCommandEvent& event);
-      void OnScaleUnit(wxCommandEvent& event);
-      void OnTransparency(wxSpinEvent& event);
-      void OnBandAnalysis(wxCommandEvent& event);
-      void OnMouseOverMap(wxCommandEvent& event);
-	   void OnSpinCurFrame(wxCommandEvent& event) ;
-	   void OnSpinCtrlTxt(wxCommandEvent& event) ;
-	   void OnSliderScroll(wxCommandEvent& event) ;
-	   void OnSTOPBtn(wxCommandEvent& event) ;
-	   void OnPAUSEBtn(wxCommandEvent& event) ;
-	   void OnPLAYBtn(wxCommandEvent& event) ;
-	   void OnChoiceFrameRate(wxCommandEvent& event) ;
-   public:
-      MicArrayAnalyzerDlg(wxWindow* parent, MicArrayAnalyzer* maa);
-      ~MicArrayAnalyzerDlg();
-   };
+	{
+		MicArrayAnalyzer* mMAA;
+		
+		int m_iCurrRulersFormat; // used for toggling...
+		
+		int curFrame;
+		bool playing;
+		float frameRateCoeff;
+		int numOfFrames;
+		
+	protected:
+		void OnOk(wxCommandEvent& event);
+		void OnCopyResultsToClipboard(wxCommandEvent& event);
+		//      void OnChangeRulersFormat(wxCommandEvent& event);
+		void OnSeparateBandAutoscale(wxCommandEvent& event);
+		//      void OnEditWatchpoints(wxCommandEvent& event);
+		void OnShowVirtMikesPos(wxCommandEvent& event);
+		void OnScaleStyle(wxCommandEvent& event);
+		void OnScaleUnit(wxCommandEvent& event);
+		void OnTransparency(wxSpinEvent& event);
+		void OnBandAnalysis(wxCommandEvent& event);
+		void OnMouseOverMap(wxCommandEvent& event);
+		void OnSpinCurFrame(wxCommandEvent& event) ;
+		void OnSpinCtrlTxt(wxCommandEvent& event) ;
+		void OnSliderScroll(wxCommandEvent& event) ;
+		void OnSTOPBtn(wxCommandEvent& event) ;
+		void OnPAUSEBtn(wxCommandEvent& event) ;
+		void OnPLAYBtn(wxCommandEvent& event) ;
+		void OnChoiceFrameRate(wxCommandEvent& event) ;
+	public:
+		MicArrayAnalyzerDlg(wxWindow* parent, MicArrayAnalyzer* maa);
+		~MicArrayAnalyzerDlg();
+	};
 
 
 
